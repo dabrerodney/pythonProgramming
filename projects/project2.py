b48d145f4ed00c0750fc2a3adbf6a940cc4fd79e
Rodney Dabre <128955508+dabrerodney@users.noreply.github.com>
2024-01-29 20:23:30 +0530

    project 2 done

--- projects/project2.py
+++ projects/project2.py
@@ -1,7 +1,14 @@
+print("********** NUMBER GUESSING GAME **********")
 import random
 
 r = random.randrange(1, 11)
 #   randrange is used to generate random numbers between the start till end-1
 #   randint is used to generate random numbers between start till end
 
-print(r)
+number = int(input("Enter the number"))
+
+if r == number:
+    print("Correct guess!!")
+else:
+    print("Wrong guess!!")
+    print("Correct number: ",r)
