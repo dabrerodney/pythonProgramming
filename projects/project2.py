d2415c81f9cfd0681385b4e3575c4b2dd7384de2
Rodney Dabre <128955508+dabrerodney@users.noreply.github.com>
2024-01-29 19:41:17 +0530

    test

---
+++ projects/project2.py
@@ -0,0 +1,7 @@
+import random
+
+r = random.randrange(1, 11)
+#   randrange is used to generate random numbers between the start till end-1
+#   randint is used to generate random numbers between start till end
+
+print(r)
